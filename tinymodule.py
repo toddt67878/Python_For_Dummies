6fc9efe04506fb2f8e1bf04a85422744ae9375bb
Todd Traylor <toddt67878@yahoo.com>
2020-03-16 22:43:55 -0500

    Second Push with Correct Files

---
+++ tinymodule.py
@@ -0,0 +1,6 @@
+def tinyfunction(x):
+    print "Testing how modules and interactve mode communicate"
+    print "You passed me the paramater", x
+    z = x**2
+    print x, "squared is", z
+    return z
---
+++ tinyscript.py
@@ -0,0 +1,3 @@
+print "Testing how scripts and interacitve mode communicate"
+x = 500
+print "The value of x is", x
